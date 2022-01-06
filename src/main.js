cf6769c147e7a3b02dc15d0988f34f32cde8128b
wlals2997 <wlals2997@naver.com>
2022-01-07 00:45:18 +0900

    Webpack,Babel 환경설정

--- src/main.js
+++ src/main.js
@@ -1,28 +1,3 @@
-import TextSign from './TextSign.js';
+import { message } from "./message";
 
-const textSign = new TextSign();
-
-const condition = true;
-// const condition = false;
-
-const promise = new Promise((resolve, reject) => {
-	if (condition) {
-		resolve('성공!');
-	} else {
-		reject('실패!');
-	}
-});
-promise
-	.then(message => {
-		console.log(message);
-		textSign.elem.innerHTML = message;
-		textSign.elem.dataset.state = 'success';
-	})
-	.catch(error => {
-		console.log(error);
-		textSign.elem.innerHTML = error;
-		textSign.elem.dataset.state = 'failure';
-	})
-	.finally(() => {
-		console.log('끝에 실행');
-	});
+message('1분코딩');
